09a7c0aecc0f73521c18f7a8422dbbfc197b9970
Pawel Kluska <pkluska@griddynamics.com>
2025-04-08 13:13:52 +0200

    sdf

--- script.sh
+++ script.sh
@@ -1,4 +1,15 @@
 git clone https://github.com/Pawel-Kluska/Pawel-Kluska-PerformanceAndScalabilityTest.git
 cd Pawel-Kluska-PerformanceAndScalabilityTest/Microservice1
 sudo apt-get install openjdk-17-jdk
-./gradlew bootRun -Dspring.profiles.active=cloud
+sudo ./gradlew bootRun --args='--spring.profiles.active=cloud'
+
+
+git clone https://github.com/Pawel-Kluska/Pawel-Kluska-PerformanceAndScalabilityTest.git
+cd Pawel-Kluska-PerformanceAndScalabilityTest/Microservice2
+sudo apt-get install openjdk-17-jdk
+sudo ./gradlew bootRun --args='--spring.profiles.active=cloud'
+
+git clone https://github.com/Pawel-Kluska/Pawel-Kluska-PerformanceAndScalabilityTest.git
+cd Pawel-Kluska-PerformanceAndScalabilityTest/Microservice3
+sudo apt-get install openjdk-17-jdk
+sudo ./gradlew bootRun --args='--spring.profiles.active=cloud'
