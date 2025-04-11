bcb29eebbb63a5508e2cfa0e5418b5d93dee7bbd
Pawel Kluska <pkluska@griddynamics.com>
2025-04-11 14:19:17 +0200

    dsf

--- script.sh
+++ script.sh
@@ -1,26 +1,27 @@
-sudo apt-get install git -y
+
 git clone https://github.com/Pawel-Kluska/Pawel-Kluska-PerformanceAndScalabilityTest.git
+sudo apt-get install openjdk-17-jdk -y
 cd Pawel-Kluska-PerformanceAndScalabilityTest/Microservice1
-sudo apt-get install openjdk-17-jdk -y
 sudo ./gradlew bootRun --args='--spring.profiles.active=cloud'
 
-sudo apt-get install git -y
+
 git clone https://github.com/Pawel-Kluska/Pawel-Kluska-PerformanceAndScalabilityTest.git
+sudo apt-get install openjdk-17-jdk -y
 cd Pawel-Kluska-PerformanceAndScalabilityTest/Microservice2
-sudo apt-get install openjdk-17-jdk -y
 sudo ./gradlew bootRun --args='--spring.profiles.active=cloud'
 
-sudo apt-get install git -y
+
 git clone https://github.com/Pawel-Kluska/Pawel-Kluska-PerformanceAndScalabilityTest.git
-cd Pawel-Kluska-PerformanceAndScalabilityTest/Microservice3
 sudo apt-get install openjdk-17-jdk -y
+cd Pawel-Kluska-PerformanceAndScalabilityTest/Microservice3
 sudo ./gradlew bootRun --args='--spring.profiles.active=cloud'
 
 git pull origin main
 sudo ./gradlew bootRun --args='--spring.profiles.active=cloud'
 
+
+
 ssh -i "aws_key_apir_magisterka.pem" ubuntu@ec2-34-201-66-242.compute-1.amazonaws.com
-ssh -i "aws_key_apir_magisterka.pem" ubuntu@34.207.161.129
 
 nginx
 http://3.82.51.92
@@ -37,6 +38,17 @@ Microservice1
 44.223.65.31
 54.205.6.32
 
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@98.81.58.221
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@54.172.161.18
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@3.83.237.134
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@18.232.180.165
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@18.234.88.126
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@54.91.94.84
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@3.90.18.8
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@44.223.65.31
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@54.205.6.32
+
+
 Microservice2
 54.163.59.104
 3.82.35.121
@@ -47,6 +59,16 @@ Microservice2
 18.215.158.172
 3.91.98.145
 13.218.222.113
+
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@54.163.59.104
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@3.82.35.121
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@54.211.34.57
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@3.80.64.25
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@3.208.31.128
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@54.197.28.153
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@18.215.158.172
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@3.91.98.145
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@13.218.222.113
 
 http://54.163.59.104,http://3.82.35.121,http://54.211.34.57,http://3.80.64.25,http://3.208.31.128,http://54.197.28.153,http://18.215.158.172,http://3.91.98.145,http://13.218.222.113
 
@@ -61,4 +83,16 @@ Microservice3
 18.209.49.224
 184.72.168.255
 
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@3.95.37.182
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@54.173.26.17
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@18.212.240.179
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@3.81.235.87
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@52.207.230.227
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@3.85.37.44
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@54.144.95.31
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@18.209.49.224
+ssh -i "aws_key_apir_magisterka.pem" ubuntu@184.72.168.255
+
 http://3.95.37.182,http://54.173.26.17,http://18.212.240.179,http://3.81.235.87,http://52.207.230.227,http://3.85.37.44,http://54.144.95.31,http://18.209.49.224,http://184.72.168.255
+
+
