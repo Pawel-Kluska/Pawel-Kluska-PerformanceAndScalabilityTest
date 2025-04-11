750067423a2f60ff3d3c23c0d315e63727aa9c58
Pawel Kluska <pkluska@griddynamics.com>
2025-04-11 13:21:18 +0200

    dsf

--- script.sh
+++ script.sh
@@ -1,14 +1,16 @@
+sudo apt-get install git -y
 git clone https://github.com/Pawel-Kluska/Pawel-Kluska-PerformanceAndScalabilityTest.git
 cd Pawel-Kluska-PerformanceAndScalabilityTest/Microservice1
 sudo apt-get install openjdk-17-jdk -y
 sudo ./gradlew bootRun --args='--spring.profiles.active=cloud'
 
-
+sudo apt-get install git -y
 git clone https://github.com/Pawel-Kluska/Pawel-Kluska-PerformanceAndScalabilityTest.git
 cd Pawel-Kluska-PerformanceAndScalabilityTest/Microservice2
 sudo apt-get install openjdk-17-jdk -y
 sudo ./gradlew bootRun --args='--spring.profiles.active=cloud'
 
+sudo apt-get install git -y
 git clone https://github.com/Pawel-Kluska/Pawel-Kluska-PerformanceAndScalabilityTest.git
 cd Pawel-Kluska-PerformanceAndScalabilityTest/Microservice3
 sudo apt-get install openjdk-17-jdk -y
@@ -21,44 +23,42 @@ ssh -i "aws_key_apir_magisterka.pem" ubuntu@ec2-34-201-66-242.compute-1.amazonaw
 ssh -i "aws_key_apir_magisterka.pem" ubuntu@34.207.161.129
 
 nginx
-http://34.201.66.242/
+http://3.82.51.92
 
 
 Microservice1
-35.171.178.202
-50.19.141.101
-52.4.157.127
-54.144.114.222
-54.152.113.76
-3.95.159.244
-44.211.165.168
-3.92.134.76
-34.207.161.129
-
-ssh -i "aws_key_apir_magisterka.pem" ubuntu@18.208.134.56
-
-
+98.81.58.221
+54.172.161.18
+3.83.237.134
+18.232.180.165
+18.234.88.126
+54.91.94.84
+3.90.18.8
+44.223.65.31
+54.205.6.32
 
 Microservice2
-54.146.190.80
-54.197.11.161
-44.203.113.240
-44.211.38.92
-54.208.165.162
-34.203.40.253
-34.238.232.133
-54.164.198.223
-18.208.134.56
+54.163.59.104
+3.82.35.121
+54.211.34.57
+3.80.64.25
+3.208.31.128
+54.197.28.153
+18.215.158.172
+3.91.98.145
+13.218.222.113
 
-ssh -i "aws_key_apir_magisterka.pem" ubuntu@35.173.125.102
+http://54.163.59.104,http://3.82.35.121,http://54.211.34.57,http://3.80.64.25,http://3.208.31.128,http://54.197.28.153,http://18.215.158.172,http://3.91.98.145,http://13.218.222.113
 
 Microservice3
-54.234.29.183
-54.166.99.239
-54.173.224.77
-52.207.92.121
-54.209.125.4
-54.175.162.84
-54.146.134.192
-3.91.208.107
-35.173.125.102
+3.95.37.182
+54.173.26.17
+18.212.240.179
+3.81.235.87
+52.207.230.227
+3.85.37.44
+54.144.95.31
+18.209.49.224
+184.72.168.255
+
+http://3.95.37.182,http://54.173.26.17,http://18.212.240.179,http://3.81.235.87,http://52.207.230.227,http://3.85.37.44,http://54.144.95.31,http://18.209.49.224,http://184.72.168.255
